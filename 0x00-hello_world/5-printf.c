bd07c3a1da2b0fc6aaba8f48fdf5a84bd41bcef6
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-16 14:23:06 -0800

    Hello Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing with proper grammar, but the outcome is a piece of art
+ * Return: 0 (if successfully compiled)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
