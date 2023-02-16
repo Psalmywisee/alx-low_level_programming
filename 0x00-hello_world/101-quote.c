b81280a4df79b862dec526e76a7967fba90f9351
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-16 15:38:15 -0800

    UNIS is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - Program that prints piece of art is usedful
+ *
+ * Return: 1 (If successful)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n", stdout);
+return (1);
+}
