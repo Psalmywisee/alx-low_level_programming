cd2e970636affa42aaa4bc5e8f023e64404973ab
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-16 16:05:06 -0800

    Unix operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-#include <unstdio.h>
+#include <stdio.h
 /**
  * main - Program that prints piece of art is usedful
  * Return: 1 (If successful)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, \2015-10-19\n", 59);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
