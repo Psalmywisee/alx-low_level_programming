801195de89835ece31a6f9eebcf72be2deaea4be
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-17 13:15:12 -0800

    Prints alphabeths inlower case from a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints the alphabet in
+ * lowercase, followed by a new line
+ * followed by a new line
+ * Return: 0 (when Successful)
+ */
+int main(void)
+{
+	char ap
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+	{
+		putchar(ap);
+	}
+	putchar('\n');
+	return (0);
+}
