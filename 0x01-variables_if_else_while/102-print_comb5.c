598f313c59a96a2b26e3338443d775921c6489b7
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-17 19:46:32 -0800

    All possible combinations from 0 to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - A program that  prints all possible combinations
+ * of two two-digit numbers from 0 - 99
+ * Return: 0 (If successful)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((xi % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
