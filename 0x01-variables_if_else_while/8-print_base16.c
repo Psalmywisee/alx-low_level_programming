181aaab929b194cdfb127a1319a691faad40cdcc
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-17 19:55:14 -0800

    Hexadecimal numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 
 /**
  * main - a program that prints all the numbers of
-   base 16 in lowercase, followed by a new line
+ * base 16 in lowercase, followed by a new line
  * Return: 0 (If successful)
  */
 int main(void)
