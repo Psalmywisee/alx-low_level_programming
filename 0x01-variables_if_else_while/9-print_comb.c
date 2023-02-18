9b1bdd13246622db9e4a286e014cc5fe8b7f4809
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-17 19:30:34 -0800

    Prints combination of digita

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - program  prints all possible combinations of single-digits numbers
+ * Return: 0 (If successful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
