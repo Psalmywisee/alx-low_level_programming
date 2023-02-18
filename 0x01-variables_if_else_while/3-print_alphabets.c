bd4b4b6393a1b29f1fe762d8fb29c615113f45e1
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-17 20:04:49 -0800

    Prints both upper and lower case alphabeths

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - print both lower and upper case alphabeth.
- * Return 0 (if Successful)
+ * main - A program that prints alphabtst in
+ * lowercase, and then in uppercase
+ * followed by a new line
+ * Return: 0 (When successful)
  */
-
 int main(void)
 {
-int alp;
-for (alp = 'a'; alp <= 'z'; alp++)
-putchar(alp);
-for (alp = 'A'; alp <= 'Z'; alp++)
-putchar(alp);
-putchar('\n');
-return (0);
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+	putchar('\n');
+	return (0);
 }
