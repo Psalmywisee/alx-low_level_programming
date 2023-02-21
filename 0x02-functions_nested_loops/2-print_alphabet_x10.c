1b71e982e58c8c2479f12fbfb805e8f074060a7f
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-21 12:50:09 -0800

    prints alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Prints alphabet ten times
+ *Return: 0 (if successful)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+		_putchar('\n');
+	}
+}
