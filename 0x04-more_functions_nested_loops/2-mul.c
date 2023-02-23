00c18caba8de452eae195fedb29ae0b007f600c8
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-23 14:20:12 -0800

    checks the digits from 0 to 9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - finds digit from 0 to 9
+*mul - checks for a digit from 0 to 9
+*@a: parameter a
+*@b: parameter b
+*Return: 0
+*/
+
+int mul(int a, int b)
+{
+
+int mul;
+mul = a * b;
+
+return (mul);
+}
