2294b2cb0b798d2da11dfff1bad32f4db31833d9
Samuel Duruibe <samuelduruibe@gmail.com>
2023-02-23 16:06:22 -0800

    prints Traiangle

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * main - check the code
 *
@@ -5,9 +7,9 @@
 */
 int main(void)
 {
-print_triangle(2);
-print_triangle(10);
-print_triangle(1);
-print_triangle(0);
-return (0);
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	return (0);
 }
