5f78de888f88e5328beb96d5394ecf8ce4b2ac3b
Samuel Duruibe <samuelduruibe@gmail.com>
2023-03-02 14:24:55 -0800

    printing integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,25 +2,25 @@
 
 /**
  * print_number - print numbers chars
- * @n: integer params
+ * @n: integer parameter
  * Return: 0
  */
 
 void print_number(int n)
 {
-	unsigned int num1;
+	unsigned int n1;
 
-	num1 = n;
+	n1 = n;
 
 	if (n < 0)
 	{
-		putchar('-');
-		num1 = -n;
+		_putchar('-');
+		n1 = -n;
 	}
 
-	if (num1 / 10 != 0)
+	if (n1 / 10 != 0)
 	{
-		print_number(num1 / 10);
+		print_number(n1 / 10);
 	}
-	putchar((num1 % 10) + '0');
+		_putchar((num1 % 10) + '0');
 }
