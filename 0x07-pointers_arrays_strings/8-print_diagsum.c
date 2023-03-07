eb52285a8f2c49eb9f348daaa74b2330f9833b57
Samuel Duruibe <samuelduruibe@gmail.com>
2023-03-06 20:10:11 -0800

    prints the sum of diagonal squares

--- 0x07-pointers_arrays_strings/8-print_diagsum.c
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -10,21 +10,20 @@
 
 void print_diagsums(int *a, int size)
 {
-	int sqr1, sqr2, x;
+	int sum1, sum2, x;
 
-	sqr1 = 0; //first square sum
-	sqr2 = 0; //second square sum
+	sqr1 = 0;
+	sqr2 = 0;
 
 	for (x = 0; x < size; x++)
 	{
-		sqr1 = sqr1 + a[x * size + x];
+		sum1 = sum1 + a[x * size + x];
 	}
 
 	for (x = size - 1; x >= 0; x--)
 	{
-		sqr2 += a[x * size + (size - x - 1)];
+		sum2 += a[x * size + (size - x - 1)];
 	}
 
-	printf("%d, %d\n", sqr1, sqr2);
-return (0);
+	printf("%d, %d\n", sum1, sum2);
 }
