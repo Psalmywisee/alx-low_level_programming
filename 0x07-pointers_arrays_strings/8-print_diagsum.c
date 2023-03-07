0a0008f36066f8461c20d357e2432a1403b95327
Samuel Duruibe <samuelduruibe@gmail.com>
2023-03-06 20:13:31 -0800

    prints the sum of diagonal squares

--- 0x07-pointers_arrays_strings/8-print_diagsum.c
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -1,28 +1,26 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_diagsums - main entry point
- * @a: one input
- * @size: two input
+ * print_diagsums - main gate
+ * @a: input parameter
+ * @size: input parameter
  * Return: 0 always
  */
-
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, x;
+	int sum1, sum2, y;
 
-	sqr1 = 0;
-	sqr2 = 0;
+	sum1 = 0;
+	sum2 = 0;
 
-	for (x = 0; x < size; x++)
+	for (y = 0; y < size; y++)
 	{
-		sum1 = sum1 + a[x * size + x];
+		sum1 = sum1 + a[y * size + y];
 	}
 
-	for (x = size - 1; x >= 0; x--)
+	for (y = size - 1; y >= 0; y--)
 	{
-		sum2 += a[x * size + (size - x - 1)];
+		sum2 += a[y * size + (size - y - 1)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
