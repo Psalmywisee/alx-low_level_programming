4c7c4039ab6a59d3b4c3f45315a45e0ae1d4c775
Samuel Duruibe <samuelduruibe@gmail.com>
2023-03-14 15:03:08 -0700

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,34 +9,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int i, ai;
+int i, j;
+	char *str;
 
 	if (s1 == NULL)
-	s1 = " ";
+	{
+		s1 = "";
+	}
 	if (s2 == NULL)
-	s2 = " ";
-	i = 0;
-	ai = 0;
-	while (s1[i] != '\0')
-	i++;
-	while (s2[ai] != '\0')
-	ai++;
-	concat = malloc(sizeof(char) * (i + ai));
-	if (concat == NULL)
-	return (NULL);
-	i = 0;
-	ai = 0;
-	while (s1[i] != '\0')
 	{
-	concat[i] = s1[i];
-	i++;
+		s2 = "";
 	}
-	while (s2[ai] != '\0')
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-	concat[i] = s2[ai];
-	i++, ai++;
 	}
-	concat[i] = '\0';
-	return (concat);
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+
+	str = (char *)  malloc(((i + j) + 1) * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	return (str);
 }
