c5e15dca5311d8a5eee263578d161e1516dbe65a
Samuel Duruibe <samuelduruibe@gmail.com>
2023-03-20 14:33:42 -0700

    prinitng elements of struc dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,10 @@
 	void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	return (0);
+	return;
 	if (d->name == NULL)
 	d->name = "(nil)";
 	if (d->owner == NULL)
 	d->owner = "(nil)";
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
 }
