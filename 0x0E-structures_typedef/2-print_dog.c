9a00f149592af074758eb4f7c114f9d28298034e
Samuel Duruibe <samuelduruibe@gmail.com>
2023-03-20 21:15:43 -0700

    A function tp print elements of struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,8 @@
 	return;
 	if (d->name == NULL)
 	d->name = "(nil)";
+	if (d->age == NULL)
+	d->age = "(nill)"
 	if (d->owner == NULL)
 	d->owner = "(nil)";
 printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
