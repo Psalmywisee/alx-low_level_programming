e5dff7d92af665a85289c902e964486accd5535a
Samuel Duruibe <samuelduruibe@gmail.com>
2023-03-22 10:21:26 -0700

    Function to search for an integer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* array_iterator - function to print each element of an array
+* @array: array keyword
+* @size: how many element is to be printed
+* @action: pointer to print in regular or hex format
+* Return: void
+*/
+	void array_iterator(int *array, size_t size, void (*action)(int))
+	{
+	unsigned int i;
+	if (array == NULL || action == NULL)
+	return;
+	for (i = 0; i < size; i++)
+	{
+	action(array[i]);
+	}
+	}
