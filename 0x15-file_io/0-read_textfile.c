1f8524c182e4ba7d1ace33612640611c93f108c4
Samuel Duruibe <samuelduruibe@gmail.com>
2023-04-10 15:03:33 -0700

    main editted

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,13 +16,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer = malloc(sizeof(char *) * letters);
 
 	if (!buffer)
-	return (0)
+	return (0);
 		if (!filename)
-			return (0)
+			return (0);
 
 	fd = open(filename, O_RDONLY,0600);
 	if (fd == -1)
-	return (0)
+	return (0);
 	t = read(fd, buffer, letters);
 	write(STDOUT_FILENO, buffer, t);
 
