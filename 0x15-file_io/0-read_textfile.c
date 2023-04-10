5b57aa6a68367d052a3f7fda5b810b7aaec68d92
Samuel Duruibe <samuelduruibe@gmail.com>
2023-04-10 16:45:21 -0700

    Maih h header

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if (!filename)
 			return (0);
 
-	fd = open(filename, O_RDONLY,0600);
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 	return (0);
 	t = read(fd, buffer, letters);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,4 +13,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *s);
 void copy_file(const char *src, const char *dest);
-#endiif
+#endif
