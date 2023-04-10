a780c5041168763cecb7011db8f974c9a5a3af46
Samuel Duruibe <samuelduruibe@gmail.com>
2023-04-10 14:56:42 -0700

    File reading operation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,10 +20,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if (!filename)
 			return (0)
 
-	fd = open(filename, O_RDONLY,0700 );
+	fd = open(filename, O_RDONLY,0600);
 	if (fd == -1)
-	return (0);
-	buffer = malloc(sizeof(char*) * letters);
+	return (0)
 	t = read(fd, buffer, letters);
 	write(STDOUT_FILENO, buffer, t);
 
