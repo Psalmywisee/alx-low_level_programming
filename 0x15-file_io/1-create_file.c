63c90b2103b8970393e685fc6538369b46effdae
Samuel Duruibe <samuelduruibe@gmail.com>
2023-04-10 14:08:14 -0700

    Creation of file as filename and truncates if file already exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int w, fd, length = 0;
+	int w, fd, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
-			length++;
+		for (len = 0; text_content[leng];)
+			len++;
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, length);
+	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 	return (-1);
